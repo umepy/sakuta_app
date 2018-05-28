01c64c391cf7b3efb9b33e69b0eddf813a5d41c9
jackee777 <c5618168@aoyama.jp>
2018-05-28 14:15:36 +0900

    get screenshots and output to html

--- controllers/images/get_images.js
+++ controllers/images/get_images.js
@@ -1,7 +1,33 @@
+//local に保存してある screenshot の取り出しと
+//html 形式への出力を行う
+
+//取得したスクリーンショットの取得
+//url_list: スクリーンショット取得済みの url の list
 function get_images(){
-    chrome.storage.local.get("image01", function(value){
-        console.log(value.image01)
-        document.write('<img src='+value.image01+' width="100%" alt="Red dot" />');
+    var url_list;
+    chrome.storage.local.get("url_list",
+        function(value){
+            url_list = value["url_list"];
+            //まだスクリーンショットを取ってない時は実行しない
+            if(typeof(url_list) != "undefined"){
+                get_shot(url_list);
+            }
+        }
+    )
+}
+
+//取得したスクリーンショットを html 形式に
+//url_list: スクリーンショット取得済みの url の list
+function get_shot(url_list){
+    var i;
+    chrome.storage.local.get(url_list, function(value){
+        for(i in url_list){
+            //スクリーンショットを正確に取れていないときは表示しない
+            if(typeof(value[url_list[i]]) != "undefined"){
+                document.write('<p>'+url_list[i]+'</p>')
+                document.write('<img src='+value[url_list[i]]+' width="100%" alt="Red dot" />');
+            }
+        }
     })
 }
 
