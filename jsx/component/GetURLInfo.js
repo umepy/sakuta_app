237577ac92bffa4bd0a1e35fdea27139efc5053b
196kakinuma <196thinline@gmail.com>
2018-06-01 18:38:10 +0900

    タイトル等のogpがなかった場合、仮文字が入るように

--- jsx/component/GetURLInfo.js
+++ jsx/component/GetURLInfo.js
@@ -25,10 +25,16 @@ export default class GetURLInfo extends React.Component {
     .then((responseText) => {
 
       var atitle=$(responseText).filter("meta[property='og:title']").attr('content')
+      var adescription=$(responseText).filter("meta[property='og:description']").attr('content')
+      var aimage =$(responseText).filter("meta[property='og:image']").attr('content')
+      if(atitle=="") atitle="No title"
+      if(adescription=="") adescription="Sorry, we could't load."
+      if(aimage=="") aimage="http://livedoor.blogimg.jp/superbabooooo/imgs/d/f/df70fcd8.png" //TODO: 山崎のファンクションcall
+      
       this.setState({
         title: atitle ,
-        description:  $(responseText).filter("meta[property='og:description']").attr('content') ,
-        image: $(responseText).filter("meta[property='og:image']").attr('content') ,
+        description:  adescription,
+        image: aimage ,
         url: this.props.url
       });
     });
