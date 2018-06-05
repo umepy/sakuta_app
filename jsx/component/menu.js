9942a73ab8c9f3cdff7d5bd56d982462f699035d
nasu <c5618167@gmail.com>
2018-06-05 12:44:37 +0900

    検索窓の追加

--- jsx/component/menu.js
+++ jsx/component/menu.js
@@ -1,8 +1,11 @@
+import SearchWindow from "./SearchWindow.js"
+
 export default class Menu extends React.Component {
   render(){
       return (
         <div class="col-sm-8" style={{backgroundColor: '#57b8ff', height: '100%'}}>
           <p>メニュー</p>
+		  <SearchWindow/>
         </div>
       )
   }
