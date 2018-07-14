0e1d7c3385d9d6f925bb6b2aa93329673eed4658
196kakinuma <196thinline@gmail.com>
2018-07-14 15:39:00 +0900

    検索窓を真ん中へ配置

--- jsx/main.js
+++ jsx/main.js
@@ -1,10 +1,12 @@
 import Timeline from './component/timeline.js'
 import Menu from './component/menu.js'
+import SearchWindow from "./component/SearchWindow.js"
 
 class Main extends React.Component {
   render(){
     return (
       <div class="container-fluid">
+        <SearchWindow/>
         <Menu/>
         <Timeline/>
       </div>
