390eded28b46f3947e5dd1a84e3def60bc64ee34
jackee777 <c5618168@aoyama.jp>
2018-07-18 03:50:01 +0900

    rendering cat and change screenshot

--- jsx/component/FavList.js
+++ jsx/component/FavList.js
@@ -4,19 +4,32 @@ import PanelContainer from './PanelContainer.js'
 const seeList =[
   {_id: '201801010011', url: 'http://www.masayoung.net/archives/693', type: "web"},
   {_id: '201801020011', url: 'https://bagelee.com/programming/react-native/api-react-native/',type:"web"},
-  {_id: '201802030022', url: 'https://developer.mozilla.org/ja/docs/Web/JavaScript/Guide/Exception_Handling_Statements/try...catch_Statement',type: "web" },
-  {_id: '201802030022', url: 'https://mail.google.com/mail/u/0/#inbox',type: "web" },
+  {_id: '201802030022', url: 'http://www.masayoung.net/archives/693',type: "web" },
+  {_id: '201802030022', url: 'http://www.masayoung.net/archives/693',type: "web" },
   {_id: '201802030022', url: 'http://www.masayoung.net/archives/693',type: "web" }
 ]
 
 export default class FavList extends React.Component {
-  render() {
-    return (
-      <div id="favList">
-        { seeList.map(see => (
-          <FavItem key={see._id} see={see} />
-        ))}
-      </div>
-    );
-  }
+    constructor(){
+        super();
+        this.state={
+            top_sites:[]
+        }
+    }
+
+    componentWillMount() {
+        get_top_site(8).then(data => {
+            this.setState({top_sites: data});
+        });
+    }
+
+    render() {
+      return (
+        <div id="favList">
+          { this.state.top_sites.map(see => (
+            <FavItem key={see._id} see={see} />
+          ))}
+        </div>
+      );
+    }
 }
