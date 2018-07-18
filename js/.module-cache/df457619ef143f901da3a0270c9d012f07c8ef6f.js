cdb673faaf15a9d676bffd1ad686b6445e022e3c
196kakinuma <196thinline@gmail.com>
2018-07-18 17:09:10 +0900

    背景を元に戻した

---
+++ js/.module-cache/df457619ef143f901da3a0270c9d012f07c8ef6f.js
@@ -0,0 +1,72 @@
+import TLList from './TLList.js'
+import TLContainer from './TLContainer.js'
+const { Tabs, TabList, Tab, TabPanel } = ReactTabs;
+
+const seeList =[
+  {_id: '201801010011', url: 'http://www.masayoung.net/archives/693', type: "web"},
+  {_id: '201801020011', url: 'https://bagelee.com/programming/react-native/api-react-native/',type:"web"},
+  {_id: '201802030022', url: 'http://www.masayoung.net/archives/693',type: "web" }
+]
+
+const memoList=
+[
+  {_id: '201801010013', url:"メモの保存先?", type:"memo"}
+]
+
+export default class Timeline extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      sees:[],
+      memos:[],
+      removeTabs:[]
+    }
+  }
+
+  componentWillMount() {
+      get_seelater()
+          .then(data => {
+          this.setState({sees: data});
+      }).catch(data =>{
+          this.setState({sees: []});
+      });
+      get_auto_remove().then(data =>{
+          this.setState({removeTabs: data});
+      }).catch(data =>{
+          this.setState({removeTabs: []});
+      });;
+  }
+
+  render(){
+    var list = this.state.sees;
+    var rmlist=this.state.removeTabs;
+
+
+    return (
+      React.createElement("div", {id: "timeLine"}, "  ", /** こっちはタイムラインの外枠組みのdiv */
+
+      React.createElement(Tabs, null, 
+        React.createElement(TabList, null, 
+          React.createElement(Tab, null), 
+          React.createElement(Tab, null)
+        ), 
+        React.createElement(TabPanel, null, 
+          React.createElement(TLContainer, null, 
+            React.createElement(TLList, {seeList: list.slice().reverse()})
+          )
+        ), 
+        React.createElement(TabPanel, null, 
+          React.createElement(TLContainer, null, 
+            React.createElement(TLList, {seeList: rmlist.slice().reverse()})
+          )
+        )
+
+      )
+        /*<PanelContainer>
+          {/* ここにコメントを入れるためのフォーム */
+          /*<TLList seeList={list} />
+        </PanelContainer>*/
+      )
+    )
+  }
+}
